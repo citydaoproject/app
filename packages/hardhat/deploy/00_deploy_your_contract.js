3e56f3e51d3c337842676c60f22163406a5a4d40
Scott Fitsimones <iscottonline@gmail.com>
2021-07-31 22:30:29 -0700

    Initialize gnosis-starter-kit

---
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -0,0 +1,37 @@
+// deploy/00_deploy_your_contract.js
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  await deploy("YourContract", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    // args: ["Hello"],
+    log: true,
+  });
+
+  await deploy("GnosisSafe", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args: [[deployer], 1, '0x0000000000000000000000000000000000000000', '0x0000000000000000000000000000000000000000', '0x0000000000000000000000000000000000000000', '0x0000000000000000000000000000000000000000', 0, '0x0000000000000000000000000000000000000000'],
+    log: true,
+  });
+
+  /*
+    // Getting a previously deployed contract
+    const YourContract = await ethers.getContract("YourContract", deployer);
+    await YourContract.setPurpose("Hello");
+    
+    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
+  */
+};
+module.exports.tags = ["YourContract", "GnosisSafe"];
+
+/*
+Tenderly verification
+let verification = await tenderly.verify({
+  name: contractName,
+  address: contractAddress,
+  network: targetNetwork,
+});
+*/
