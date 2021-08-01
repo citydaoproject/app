76e9a2eb755d122ea24958d90f3fb09ae8f14fb9
Scott Fitsimones <iscottonline@gmail.com>
2021-07-31 22:30:27 -0700

    Initialize circom-starter-kit

---
+++ packages/hardhat/contracts/YourContract.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.6.11 < 0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "./Verifier_init.sol";
+
+contract YourContract is Verifier {
+
+  //event SetPurpose(address sender, string purpose);
+
+  string public purpose = "Building Unstoppable Apps";
+
+  constructor() public {
+    // what should we do on deploy?
+  }
+
+  function setPurpose(string memory newPurpose) public {
+      purpose = newPurpose;
+      console.log(msg.sender,"set purpose to",purpose);
+      //emit SetPurpose(msg.sender, purpose);
+  }
+}
