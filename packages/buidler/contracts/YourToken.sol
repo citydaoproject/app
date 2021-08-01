547b40fc6cc763e933b320975078852c555129ed
Scott Fitsimones <iscottonline@gmail.com>
2021-07-31 22:27:11 -0700

    Initialize token-dev

---
+++ packages/buidler/contracts/YourToken.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.6.0 <0.7.0;
+
+import "@nomiclabs/buidler/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract YourToken is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("YourToken", "YTK") {
+        _mint(msg.sender, initialSupply);
+    }
+}
