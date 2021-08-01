f2330f0e4cb51e2f54a017a0a4b5e55cbb435c71
Scott Fitsimones <iscottonline@gmail.com>
2021-07-31 22:27:30 -0700

    Initialize new-contract-hook

---
+++ packages/buidler/contracts/YourContract.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.6.0 <0.7.0;
+
+import "@nomiclabs/buidler/console.sol";
+
+contract YourContract {
+
+  event SetPurpose(address sender, string purpose);
+
+  string public purpose;
+
+  constructor() public {
+    setPurpose("🛠 Programming Unstoppable Money");
+  }
+
+  function setPurpose(string memory newPurpose) public {
+    purpose = newPurpose;
+    console.log(msg.sender,"set purpose to",purpose);
+    emit SetPurpose(msg.sender, purpose);
+  }
+
+}
