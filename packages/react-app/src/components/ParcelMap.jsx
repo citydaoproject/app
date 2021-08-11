7cecc98637a678b4c486827d65ea579f26211eb1
gregrolwes <gregory.rolwes@slu.edu>
2021-08-11 16:29:45 -0500

    Added parcel overlay from JSON

--- packages/react-app/src/components/ParcelMap.jsx
+++ packages/react-app/src/components/ParcelMap.jsx
@@ -1,10 +1,6 @@
-import { Button } from "antd";
 import React, { useRef, useEffect, useState } from "react";
-import { useThemeSwitcher } from "react-css-theme-switcher";
-import Address from "./Address";
-import Balance from "./Balance";
-import Wallet from "./Wallet";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import Parcel0 from "../temp_data/Parcel0.json";
 
 mapboxgl.accessToken = "pk.eyJ1IjoiZ3JlZ3JvbHdlcyIsImEiOiJja3J1cnhvbWEwMGQxMnZ0NjJ4OW80emZ6In0.XPrRJMSMXwdIC6k83O4lew";
 
@@ -48,6 +44,35 @@ export default function ParcelMap() {
   const [lng, setLng] = useState(-106.331);
   const [lat, setLat] = useState(43.172);
   const [zoom, setZoom] = useState(5);
+  const [parcels, setParcels] = useState([Parcel0]);
+
+  const addParcelToMap = (geojson, parcel_id) => {
+    console.log(parcel_id);
+    map.current.addSource(parcel_id, {
+      type: "geojson",
+      data: geojson,
+    });
+    map.current.addLayer({
+      id: `${parcel_id}_fill`,
+      source: parcel_id,
+      type: "fill",
+      paint: {
+        "fill-color": "#eff551",
+        "fill-opacity": 0.2,
+        "fill-outline-color": "#eff551",
+      },
+    });
+    // add parcel outline
+    map.current.addLayer({
+      id: `${parcel_id}_outline`,
+      source: parcel_id,
+      type: "line",
+      paint: {
+        "line-color": "#eff551",
+        "line-width": 5,
+      },
+    });
+  };
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -56,6 +81,11 @@ export default function ParcelMap() {
       style: "mapbox://styles/gregrolwes/ckruwxiwk20cf18o0x218571b",
       center: [lng, lat],
       zoom: zoom,
+    });
+    map.current.on("load", function () {
+      parcels.forEach((parcel, parcel_idx) => {
+        addParcelToMap(parcel, `parcel${parcel_idx}`);
+      });
     });
   });
 
