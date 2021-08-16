e567ef8a94468777f03c561d5810091f539d2f3b
gregrolwes <gregory.rolwes@slu.edu>
2021-08-16 16:48:20 -0500

    Rename contract to CityDaoParcel

--- packages/hardhat/contracts/CityDaoParcel.sol
+++ packages/hardhat/contracts/CityDaoParcel.sol
@@ -1,15 +1,13 @@
 pragma solidity >=0.6.0 <0.7.0;
 //SPDX-License-Identifier: MIT
 
-//import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
+import "@openzeppelin/contracts/access/Ownable.sol"//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
 
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
 
-contract Parcel is ERC721, Ownable {
+contract CityDaoParcel is ERC721, Ownable {
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
