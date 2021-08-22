c89c3107aae7ad6e4e08965970c6a1769f5c04df
gregrolwes <gregory.rolwes@slu.edu>
2021-08-21 21:59:03 -0500

    Added getListedParcels

--- packages/hardhat/contracts/CityDaoParcel.sol
+++ packages/hardhat/contracts/CityDaoParcel.sol
@@ -7,13 +7,14 @@ import "@openzeppelin/contracts/access/Ownable.sol"; //learn more: https://docs.
 
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
 
-contract CityDaoParcel is ERC721, Ownable, ERC721Tradable {
+contract CityDaoParcel is ERC721, Ownable {
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  mapping(uint256 => string) public parcelIdToTokenURI;
-  mapping(uint256 => string) public parcelIdToPrice;
+  mapping(uint256 => string) private _parcelIdToTokenURI;
+  mapping(uint256 => string) private _parcelIdToPrice;
+  uint256[] private _parcelIds = [];
 
   constructor() public ERC721("CityDaoParcel", "YCB") {
     _setBaseURI("https://ipfs.io/ipfs/");
@@ -22,8 +23,9 @@ contract CityDaoParcel is ERC721, Ownable, ERC721Tradable {
   function listParcel(string memory tokenURI, string memory price) public onlyOwner returns (uint256) {
     uint256 parcelId = _tokenIds.current();
     _tokenIds.increment();
-    parcelIdToTokenURI[parcelId] = tokenURI;
-    parcelIdToPrice[parcelId] = price;
+    _parcelIdToTokenURI[parcelId] = tokenURI;
+    _parcelIdToPrice[parcelId] = price;
+    _parcelIds.push(parcelId);
 
     return parcelId;
   }
@@ -34,12 +36,20 @@ contract CityDaoParcel is ERC721, Ownable, ERC721Tradable {
       returns (uint256)
   {
       _safeMint(_toAddress, parcelId);
-      _setTokenURI(parcelId, parcelIdToTokenURI[parcelId]);
+      _setTokenURI(parcelId, _parcelIdToTokenURI[parcelId]);
 
       return parcelId;
   }
 
-  function getListedParcels() public view returns (mapping(uint256 => string)) {
-    return parcelIdToTokenURI;
+  function getListedParcels() public view returns (string[] memory) {
+    string[] memory ret = new string[](_parcelIds.length);
+    for (uint i = 0; i < _parcelIds.length; i++) {
+        ret[i] = _parcelIdToTokenURI[i];
+    }
+    return ret;
+  }
+
+  function getParcelIds() public view returns (uint256[] memory) {
+    return _parcelIds;
   }
 }
