dd943947f8ba2bc464eba025475fec2e0cfe0282
Uwe Cerron <uwe@hackd.com>
2021-08-22 21:52:03 -0400

    Royalties: testing royalties smart contract

--- packages/hardhat/contracts/Parcel.sol
+++ packages/hardhat/contracts/Parcel.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.6.0 <0.7.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 //learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
 
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
@@ -13,9 +14,22 @@ contract Parcel is ERC721, Ownable {
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
+  address public cityDao;
+  address public txFeeToken;
+  uint public txFeeAmount;
+  //only city dao designated addresses will receive royalty
+  mapping(address => bool) public excludedList; 
 
-  constructor() public ERC721("Parcel", "YCB") {
+  constructor(   
+    address _cityDao, 
+    address _txFeeToken,
+    uint _txFeeAmount) public ERC721("Parcel", "YCB") {
     _setBaseURI("https://ipfs.io/ipfs/");
+    cityDao = _cityDao;
+    txFeeToken = _txFeeToken;
+    txFeeAmount = _txFeeAmount/ 100 * 2;
+    excludedList[_cityDao] = true; 
+    _mint(cityDao, 0);
   }
 
   function mintItem(address to, string memory tokenURI)
@@ -30,5 +44,56 @@ contract Parcel is ERC721, Ownable {
       _setTokenURI(id, tokenURI);
 
       return id;
+  }
+  function setExcluded(address excluded, bool status) external {
+    require(msg.sender == cityDao, 'city dao only');
+    excludedList[excluded] = status;
+  }
+
+  function transferFrom(
+    address from, 
+    address to, 
+    uint256 tokenId
+  ) public override {
+     require(
+       _isApprovedOrOwner(_msgSender(), tokenId), 
+       'ERC721: transfer caller is not owner nor approved'
+     );
+     if(excludedList[from] == false) {
+      _payTxFee(from);
+     }
+     _transfer(from, to, tokenId);
+  }
+
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 tokenId
+   ) public override {
+     if(excludedList[from] == false) {
+       _payTxFee(from);
+     }
+     safeTransferFrom(from, to, tokenId, '');
+   }
+
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 tokenId,
+    bytes memory _data
+  ) public override {
+    require(
+      _isApprovedOrOwner(_msgSender(), tokenId), 
+      'ERC721: transfer caller is not owner nor approved'
+    );
+    if(excludedList[from] == false) {
+      _payTxFee(from);
+    }
+    _safeTransfer(from, to, tokenId, _data);
+  }
+
+  function _payTxFee(address from) internal {
+    IERC20 token = IERC20(txFeeToken);
+    token.transferFrom(from, cityDao, txFeeAmount);
   }
 }
