314ebb30c8c194bec86be8758f908e0ce140c222
Greg <rolwesg@gmail.com>
2021-09-08 18:35:27 -0500

    Feature: Add ant design theme files Fixes #25

--- packages/react-app/craco.config.js
+++ packages/react-app/craco.config.js
@@ -1,5 +1,19 @@
 // craco.config.js
+const CracoLessPlugin = require("craco-less");
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { "@primary-color": "#000000" },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -4,14 +4,15 @@ import { useDispatch } from "react-redux";
 import Web3Wrapper from "./Web3Wrapper";
 import { setDebugMode } from "./actions";
 
-import "antd/dist/antd.css";
-import "./App.css";
+import "./App.less";
+import { Button } from "antd";
 
 function App() {
   const dispatch = useDispatch();
   dispatch(setDebugMode(true));
   return (
     <div className="App text-center">
+      <Button type="primary">Hey there!</Button>
       <Web3Wrapper />
     </div>
   );
