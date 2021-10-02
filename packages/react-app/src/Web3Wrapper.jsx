495e62a8782dc4997e447b5614c84d96fd7db7a0
Greg <rolwesg@gmail.com>
2021-10-02 15:06:50 -0500

    Feature: Connect to MetaMask button (#60)

--- packages/react-app/src/Web3Wrapper.jsx
+++ packages/react-app/src/Web3Wrapper.jsx
@@ -1,14 +1,13 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Web3Modal from "web3modal";
 import { useSelector, useDispatch } from "react-redux";
 
 import { setExchangePrice, setGasPrice } from "./actions";
-import { logoutOfWeb3Modal } from "./helpers";
 import { Wallet } from "./components";
 import { INFURA_ID, NETWORKS } from "./constants";
-import { useExchangePrice, useGasPrice, useUserSigner } from "./hooks";
+import { useExchangePrice, useGasPrice } from "./hooks";
 import { BrowsePlots } from "./views";
 
 const { ethers } = require("ethers");
@@ -42,13 +41,10 @@ const web3Modal = new Web3Modal({
 });
 
 function Web3Wrapper() {
-  // injecedProvider will be used when metamask connection is implemented
-  const [injectedProvider, setInjectedProvider] = useState();
-
   const userAddress = useSelector(state => state.user.address);
   const price = useSelector(state => state.network.exchangePrice);
   const DEBUG = useSelector(state => state.debug.debug);
-  useUserSigner(injectedProvider); // initialize signer
+
   const dispatch = useDispatch();
 
   const mainnetProvider = mainnetInfura;
@@ -56,38 +52,12 @@ function Web3Wrapper() {
   dispatch(setExchangePrice(useExchangePrice(targetNetwork, mainnetProvider)));
   dispatch(setGasPrice(useGasPrice(targetNetwork, "fast")));
 
-  const loadWeb3Modal = useCallback(async () => {
-    const provider = await web3Modal.connect();
-
-    setInjectedProvider(new ethers.providers.Web3Provider(provider));
-
-    provider.on("chainChanged", chainId => {
-      DEBUG && console.log(`chain changed to ${chainId}! updating providers`);
-      setInjectedProvider(new ethers.providers.Web3Provider(provider));
-    });
-
-    provider.on("accountsChanged", () => {
-      DEBUG && console.log(`account changed!`);
-      setInjectedProvider(new ethers.providers.Web3Provider(provider));
-    });
-
-    // Subscribe to session disconnection
-    provider.on("disconnect", (code, reason) => {
-      DEBUG && console.log(code, reason);
-      logoutOfWeb3Modal(web3Modal);
-    });
-  }, [setInjectedProvider, DEBUG]);
-
-  useEffect(() => {
-    loadWeb3Modal();
-  }, [loadWeb3Modal]);
-
   return (
     <div className="Web3Wrapper flex flex-col flex-grow">
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <BrowsePlots injectedProvider={injectedProvider} networkProvider={networkProvider} />
+            <BrowsePlots web3Modal={web3Modal} networkProvider={networkProvider} />
           </Route>
         </Switch>
       </BrowserRouter>
--- packages/react-app/src/components/PlotTabs.tsx
+++ packages/react-app/src/components/PlotTabs.tsx
@@ -15,11 +15,10 @@ export default function PlotTabs() {
   const plots = useAppSelector((state: RootState) => state.plots.plots).filter(plot => {
     return stringifyPlotId(plot.id).includes(idFilter ?? "");
   });
-  console.log(fetchingPlots);
 
   return (
     <Tabs defaultActiveKey="1" className="px-4 w-full overflow-visible">
-      <TabPane tab="Remaining" key="1">
+      <TabPane tab="Available" key="1">
         <PlotList
           plots={plots.filter(plot => !plot.sold)}
           emptyMessage={fetchingPlots ? "Loading..." : "We're sold out! Be on the lookout for the next drop."}
@@ -32,10 +31,12 @@ export default function PlotTabs() {
         />
       </TabPane>
       <TabPane tab="Your Land" key="3">
-        <PlotList
-          plots={plots.filter(plot => plot.owner === userAddress && userAddress)}
-          emptyMessage={fetchingPlots ? "Loading..." : "You don't have any plots yet."}
-        />
+        {userAddress && (
+          <PlotList
+            plots={plots.filter(plot => plot.owner === userAddress)}
+            emptyMessage={fetchingPlots ? "Loading..." : "You don't have any plots yet."}
+          />
+        )}
         {!userAddress && <span className="text-gray-7 third-font">Connect your wallet to see your owned plots</span>}
       </TabPane>
     </Tabs>
