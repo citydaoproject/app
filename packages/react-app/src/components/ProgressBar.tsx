1fb4bd07dc7acd067b29dc8b2eb0f5ab7fa93669
Greg <gregory.rolwes@slu.edu>
2021-10-01 23:46:18 -0500

    Miscellaneous-fixes (#55)

--- packages/react-app/src/components/ProgressBar.tsx
+++ packages/react-app/src/components/ProgressBar.tsx
@@ -6,7 +6,8 @@ export default function ProgressBar() {
   const percent_sold = Math.floor((plots.filter(plot => plot.sold).length / plots.length) * 100);
   return (
     <div className="header w-full bg-gradient h-12 z-10 text-gray-1 flex items-center justify-center text-base primary-font font-semibold">
-      {percent_sold < 100 ? "Parcel 0 sale is live!" : "Parcel 0 sale is sold out!"}
+      {plots.length > 0 && (percent_sold < 100 ? "Parcel 0 sale is live!" : "Parcel 0 sale is sold out!")}
+      {plots.length === 0 && "Fetching plots..."}
       {percent_sold < 100 ? (
         <div className="ml-12 bg-transparent flex text-gray-1 items-center text-sm font-normal secondary-font">
           <div className="progress-bar w-40 bg-gray-9 rounded-full h-2 mx-2">
