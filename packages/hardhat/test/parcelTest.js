26e44a29a115f46fe8b604d277a4cd3d2d9fa647
Lonis <52952537+Lonisss@users.noreply.github.com>
2021-11-20 15:57:14 +0200

    Add tests (#80)
    * add legal popup
    
    * clean
    
    * merge
    
    * add lorem ipsum
    
    * initial
    
    * inital tests
    
    * add test
    
    * add test cases
    
    * clean
    
    * add royalties test cases
    
    * rename, clean
    
    * add robustness to purchase test
    
    Co-authored-by: Lonis Hamaili <lonis@Loniss-MBP.lan>

---
+++ packages/hardhat/test/parcelTest.js
@@ -0,0 +1,120 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable one-var */
+const { ethers } = require("hardhat");
+const { use, expect } = require("chai");
+require("chai/register-should");
+const { solidity } = require("ethereum-waffle");
+const createTest = require("./testCreatePlots");
+const plots = require("../scripts/plots.json");
+const ipfsAPI = require("ipfs-http-client");
+const ipfs = ipfsAPI({
+  host: "ipfs.infura.io",
+  port: "5001",
+  protocol: "https",
+});
+
+use(solidity);
+
+describe("Token parcelContract", function () {
+  this.timeout(100000);
+  let parcelContract,
+    ParcelContract,
+    owner,
+    passedOwner,
+    CitizenNFTContract,
+    citizenNFTContract;
+
+  beforeEach(async () => {
+    [owner] = await ethers.getSigners();
+    ParcelContract = await ethers.getContractFactory("CityDaoParcel");
+    parcelContract = await ParcelContract.deploy();
+    passedOwner = owner.address;
+    if (this.ctx.currentTest.title === "Plots created properly?") return;
+    if (this.ctx.currentTest.title === "Owner privileges enforced") {
+      passedOwner = "0xb40A70Aa5C30215c44F27BF990cBf4D3E5Acb384"; // random address
+    }
+    await createTest.createTest(parcelContract, passedOwner);
+
+    CitizenNFTContract = await ethers.getContractFactory("CitizenNFT");
+    citizenNFTContract = await CitizenNFTContract.deploy();
+  });
+
+  describe("Purchasing land", () => {
+    const ID = 1;
+    it("should create plots properly", async () => {
+      const price = `100000000000000000`;
+      let idx = 0;
+      for (let plot = plots.plots; idx < 2; idx++) {
+        const plotUri = await ipfs.add(
+          JSON.stringify({
+            name: `CityDAO Parcel 0, Plot ${idx}`,
+            description: "NFT test",
+            image: "https://media0.giphy.com/media/Ju7l5y9osyymQ/200.gif",
+            terrain: "Mountainous",
+            sqft: "~1750 sqft",
+            geojson: plot,
+          })
+        );
+        await parcelContract.createPlot(price, plotUri);
+        const plotID = idx + 1;
+        expect((await parcelContract.getPrice(plotID)).toString()).to.equal(
+          price
+        );
+        expect((await parcelContract.getPlotIds()).toString()).to.include(
+          plotID
+        );
+      }
+    });
+
+    it("should enforce owner privileges", async () => {
+      const err = "Ownable: caller is not the owner";
+      await expect(
+        parcelContract.setRoyalty(owner.address, 100)
+      ).to.be.revertedWith(err);
+      await expect(
+        parcelContract.createPlot(
+          ethers.BigNumber.from(`${100000000000000000 * 1}`),
+          {
+            gasLimit: 400000,
+          }
+        )
+      ).to.be.revertedWith(err);
+      await expect(
+        parcelContract.whitelistAddress(owner.address, true)
+      ).to.be.revertedWith(err);
+    });
+
+    it("should enforce whitelisting", async () => {
+      const price = await parcelContract.getPrice(ID);
+      await expect(
+        parcelContract.buyPlot(ID, { value: price })
+      ).to.be.revertedWith(
+        "You don't have the right citizen NFT to buy this plot yet."
+      );
+      await parcelContract.whitelistAddress(owner.address, true);
+      expect(await parcelContract.isWhitelisted(owner.address)).to.be.true;
+    });
+
+    it("should complete purchase && double spend not possible", async () => {
+      parcelContract.whitelistAddress(owner.address, true);
+      const price = await parcelContract.getPrice(ID);
+      await parcelContract.buyPlot(ID, { value: price });
+      expect(
+        (await parcelContract.getAllSoldStatus()).filter((i) => i === true)
+      ).to.have.lengthOf(1);
+      const buyID = (await parcelContract.getAllSoldStatus()).indexOf(true) + 1;
+      expect(await parcelContract.isSold(buyID)).to.be.true;
+      expect(async () =>
+        parcelContract
+          .buyPlot(buyID)
+          .to.throw("This plot has already been sold!")
+      );
+      expect((await parcelContract.getOwners())[buyID - 1]).to.equal(
+        owner.address
+      );
+    });
+  });
+});
---
+++ packages/hardhat/test/testCreatePlots.js
@@ -0,0 +1,111 @@
+/* eslint no-use-before-define: "warn" */
+const plots = require("../scripts/plots.json");
+const parcel = require("../scripts/parcel.json");
+const communal = require("../scripts/communal.json");
+const { ethers } = require("hardhat");
+const ipfsAPI = require("ipfs-http-client");
+const ipfs = ipfsAPI({
+  host: "ipfs.infura.io",
+  port: "5001",
+  protocol: "https",
+});
+
+const delayMS = 1000; // increase to 5000ms if rate-limited
+
+const createTest = async (parcelContract, owner) => {
+  // ADDRESS TO MINT TO:
+  let toAddress = "0xb40A70Aa5C30215c44F27BF990cBf4D3E5Acb384"; // this will be the temporary address to hold the plots on testnets, in practice will be owned by CityDAO
+  toAddress = owner;
+
+  console.log("\n\n 🎫 Minting to " + toAddress + "...\n");
+
+  // royalty
+  await parcelContract.setRoyalty(
+    "0x2C68489f711eEf3e30fC0Cc20Bdaa436A3b4cc4a",
+    100
+  );
+  const royaltyPrice = ethers.BigNumber.from("1000000000000000000");
+  const [addr, amount] = await parcelContract.royaltyInfo(1, royaltyPrice);
+  console.log(
+    `${ethers.utils.formatEther(royaltyPrice)} ETH royalty pays`,
+    ethers.utils.formatEther(amount)
+  );
+
+  // whitelist
+  await parcelContract.setCitizenNftContract(
+    "0xc5a5C42992dECbae36851359345FE25997F5C42d"
+  );
+  await parcelContract.setCitizenNftIds([7, 42, 69]);
+  await parcelContract.whitelistAddress(
+    "0x2C68489f711eEf3e30fC0Cc20Bdaa436A3b4cc4a",
+    true
+  );
+  const whitelisted = await parcelContract.isWhitelisted(
+    "0x2C68489f711eEf3e30fC0Cc20Bdaa436A3b4cc4a"
+  );
+  console.log("Whitelisted: " + whitelisted);
+
+  try {
+    const parcelUri = await ipfs.add(JSON.stringify({ plots: parcel.parcel }));
+    await sleep(delayMS);
+    await parcelContract.setParcelMetadata(parcelUri.path);
+    const plotsUri = await ipfs.add(JSON.stringify({ plots: plots.plots }));
+    await sleep(delayMS);
+    await parcelContract.setPlotsMetadata(plotsUri.path);
+    const communalUri = await ipfs.add(
+      JSON.stringify({ features: communal.features })
+    );
+    await sleep(delayMS);
+    await parcelContract.setCommunalLandMetadata(communalUri.path);
+    console.log(`Posting IPFS hash (${plotsUri.path})`);
+    let idx = 0;
+    for (const plot of plots.plots) {
+      if (idx > 2) break;
+      const plotUri = await ipfs.add(
+        JSON.stringify({
+          name: `CityDAO Parcel 0, Plot ${idx}`,
+          description: "Test NFT description",
+          image: "https://media0.giphy.com/media/Ju7l5y9osyymQ/200.gif",
+          terrain: "Mountainous",
+          sqft: "~1750 sqft",
+          geojson: plot,
+        })
+      );
+      await createPlot(plot, idx, plotUri.path, parcelContract);
+      await sleep(delayMS);
+      idx++;
+    }
+    await parcelContract.transferOwnership(toAddress, {
+      gasLimit: 400000,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  // transfer contract to CityDAO
+  console.log(
+    "Transferring ownership of CityDAO Plot Contract to " + toAddress + "..."
+  );
+
+  console.log("\n\n 🎫 Done!\n");
+  console.log(await parcelContract.getPlotIds());
+};
+
+async function createPlot(plot, idx, plotUri, contract) {
+  console.log(`Uploading plot${idx}...`);
+
+  await contract.createPlot(
+    ethers.BigNumber.from(`${100000000000000000 * (idx + 1)}`),
+    plotUri,
+    {
+      gasLimit: 400000,
+    }
+  );
+  await sleep(1500);
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+module.exports = { createTest };
