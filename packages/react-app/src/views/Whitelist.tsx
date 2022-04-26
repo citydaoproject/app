9be77948f1d136b500a9ad6f8fc88cd54207f68a
Valtteri Kodisto <valtteri.kodisto@megin.fi>
2022-04-26 23:09:34 +0300

    Support coinbase wallet, binance chain wallet and torus

--- packages/react-app/src/views/Whitelist.tsx
+++ packages/react-app/src/views/Whitelist.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { toast } from "react-toastify";
 
 import { useContractLoader, useAppSelector, useUserSigner } from "../hooks";
 import { ConnectWalletButton } from "../components";
@@ -32,25 +33,29 @@ export default function Whitelist({ networkProvider, web3Modal }: Props) {
   }, [contracts, networkProvider, userAddress]);
 
   const loadWeb3Modal = useCallback(async () => {
-    const provider = await web3Modal.connect();
+    try {
+      const provider = await web3Modal.connect();
 
-    setInjectedProvider(new ethers.providers.Web3Provider(provider));
-
-    provider.on("chainChanged", (chainId: string) => {
-      DEBUG && console.log(`chain changed to ${chainId}! updating providers`);
       setInjectedProvider(new ethers.providers.Web3Provider(provider));
-    });
 
-    provider.on("accountsChanged", () => {
-      DEBUG && console.log(`account changed!`);
-      setInjectedProvider(new ethers.providers.Web3Provider(provider));
-    });
+      provider.on("chainChanged", (chainId: string) => {
+        DEBUG && console.log(`chain changed to ${chainId}! updating providers`);
+        setInjectedProvider(new ethers.providers.Web3Provider(provider));
+      });
 
-    // Subscribe to session disconnection
-    provider.on("disconnect", (code: string, reason: string) => {
-      DEBUG && console.log(code, reason);
-      logoutOfWeb3Modal(web3Modal);
-    });
+      provider.on("accountsChanged", () => {
+        DEBUG && console.log(`account changed!`);
+        setInjectedProvider(new ethers.providers.Web3Provider(provider));
+      });
+
+      // Subscribe to session disconnection
+      provider.on("disconnect", (code: string, reason: string) => {
+        DEBUG && console.log(code, reason);
+        logoutOfWeb3Modal(web3Modal);
+      });
+    } catch (error) {
+      toast.error("Error connecting to a wallet");
+    }
   }, [setInjectedProvider, DEBUG]);
 
   return (
