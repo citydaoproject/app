1036adc0690208cc5fe1b097cee7fce02fc82efe
Denis Rodman <blackeagle0188@gmail.com>
2022-06-17 01:32:39 +0900

    add button in mapbox popup

--- packages/react-app/src/components/PlotMap.jsx
+++ packages/react-app/src/components/PlotMap.jsx
@@ -7,6 +7,7 @@ import { useAppSelector } from "../hooks";
 import { AnimatePresence, motion } from "framer-motion";
 import { stringifyPlotId } from "../helpers/stringifyPlotId";
 import { plotsList } from "../data";
+import { PARCEL_OPENSEA } from "../constants";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
@@ -43,14 +44,16 @@ export default function PlotMap({ parcel, plots, startingCoordinates, startingZo
         }
       })
       popupContent += "</div>";
+      const openseaBtn = "<button class='view-plot-btn btn w-full' id='view_opensea'>View on Opensea</button>";
+      popupContent += openseaBtn;
       popupContent += "</div>";
 
       const lats = coordinates.map((codinate) => codinate[0]);
       const lngs = coordinates.map((codinate) => codinate[1]);
       const centerLat = (Math.min(...lats) + Math.max(...lats)) / 2;
       const centerLng = (Math.min(...lngs) + Math.max(...lngs)) / 2;
-
       popup.setLngLat([centerLat, centerLng]).setHTML(popupTitle + popupContent).addTo(map.current);
+      document.getElementById('view_opensea').addEventListener('click', () => window.open(PARCEL_OPENSEA + activePlot.id, "_blank"));
     } else {
       const popups = document.getElementsByClassName("mapboxgl-popup");
       if (popups.length) {
