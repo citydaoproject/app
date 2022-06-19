5a97cf099fffe43e91d9da0fb1a31733857da267
sdsr <sdsr.cd.dev@gmail.com>
2022-06-19 15:23:13 -0500

    Remove ProgressBar and make adjustments for small screens

--- packages/react-app/src/components/SensorHeaderInfo.tsx
+++ packages/react-app/src/components/SensorHeaderInfo.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 const SensorHeaderInfo = () => {
-  return <div className="bg-gray-1 text-green-1">Parcel 0</div>;
+  return <div className="sensor-header-info bg-gray-1 text-green-1">Parcel 0</div>;
 };
 
 export default SensorHeaderInfo;
