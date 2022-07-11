5ed358b967a57d077fd1c09019077acbac3d4cca
Tuncay Kaplan <trkaplan@gmail.com>
2022-07-11 13:56:38 +0300

    #113 update # of plots on wallet change + conditionally display "YOUR PLOTS(n)"

--- packages/react-app/src/components/Header.tsx
+++ packages/react-app/src/components/Header.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { ConnectWalletButton } from ".";
-import { useAppSelector } from "../hooks";
-import { RootState } from "../store";
-
 interface Props {
   connectWallet: () => void;
   userNft: number;
+  isParcelCountRead: boolean;
 }
-export default function Header({ connectWallet, userNft }: Props) {
-  const userAddress = useAppSelector((state: RootState) => state.user.address);
+export default function Header({ connectWallet, userNft, isParcelCountRead }: Props) {
   return (
     <div className="connect-wallet-section header border-b primary-font text-xl flex h-16 lg:px-4 items-center">
-      {userAddress && (
-        <span className="bg-gray-1 opacity-75 text-lg secondary-font mr-5">YOUR PLOTS ({userNft})</span>
+      {isParcelCountRead && (
+        <span className="bg-gray-1 opacity-75 text-lg secondary-font mr-5">
+          {userNft > 0 ? `YOUR PLOTS (${userNft})` : `No plots`}
+        </span>
       )}
       <ConnectWalletButton onClick={connectWallet} />
     </div>
--- packages/react-app/src/components/SidePanel.tsx
+++ packages/react-app/src/components/SidePanel.tsx
@@ -4,80 +4,76 @@ import { RootState } from "../store";
 import { useAppSelector } from "../hooks";
 import PlotList from "./PlotList";
 import { stringifyPlotId } from "../helpers/stringifyPlotId";
-import FilterNote from "./FilterNote";
 import { plotsList } from "../data";
 import { Plot } from "../models/Plot";
-import { LocationDetail, TerrainDetail, PlotDetail } from "./"
+import { LocationDetail, TerrainDetail, PlotDetail } from "./";
 
 interface Props {
-    contracts: any;
-    injectedProvider: any;
-    mainnetProvider: any;
-    userNft: Array<number>;
+  contracts: any;
+  injectedProvider: any;
+  mainnetProvider: any;
+  userNft: Array<number>;
 }
 
 export default function SidePanel({ contracts, injectedProvider, mainnetProvider, userNft }: Props) {
-    const userAddress = useAppSelector((state: RootState) => state.user.address);
-    const idFilter = useAppSelector((state: RootState) => state.plots.idFilter);
-    const activePlot = useAppSelector(state => state.plots.activePlot);
-    const [newPlots, setNewPlots] = useState<Plot[]>([])
-    const [newPlotsNum, setNewPlotsNum] = useState(0);
+  const userAddress = useAppSelector((state: RootState) => state.user.address);
+  const idFilter = useAppSelector((state: RootState) => state.plots.idFilter);
+  const activePlot = useAppSelector(state => state.plots.activePlot);
+  const [newPlots, setNewPlots] = useState<Plot[]>([]);
+  const [newPlotsNum, setNewPlotsNum] = useState(0);
 
-    useEffect(() => {
-        let plotData = plotsList.features as Plot[];
-        plotData.map((plot: Plot, index: number) => {
-            if (plot["id"]) {
-                return;
-            }
-            plot["id"] = index + 1;
-            return;
-        })
-        setNewPlots(plotData)
-    }, [])
+  useEffect(() => {
+    const plotData = plotsList.features as Plot[];
+    plotData.map((plot: Plot, index: number) => {
+      if (plot["id"]) {
+        return;
+      }
+      plot["id"] = index + 1;
+      return;
+    });
+    setNewPlots(plotData);
+  }, []);
 
-    useEffect(() => {
-        let plotData = plotsList.features as Plot[];
-        let filteredPlot = [];
-        filteredPlot = plotData.filter(plot => {
-            return stringifyPlotId(plot.id).includes(idFilter ?? "");
-        })
-        setNewPlots(filteredPlot)
-        setNewPlotsNum(filteredPlot.length);
-    }, [idFilter])
+  useEffect(() => {
+    const plotData = plotsList.features as Plot[];
+    let filteredPlot = [];
+    filteredPlot = plotData.filter(plot => {
+      return stringifyPlotId(plot.id).includes(idFilter ?? "");
+    });
+    setNewPlots(filteredPlot);
+    setNewPlotsNum(filteredPlot.length);
+  }, [idFilter]);
 
-    useEffect(() => {
-        const filteredArray = newPlots.filter(item => userNft.includes(item.id));
-        console.log(filteredArray)
-    }, [userNft, newPlots])
+  useEffect(() => {
+    const filteredArray = newPlots.filter(item => userNft.includes(item.id));
+  }, [userNft, newPlots]);
 
-    return (
-        <div className="plot-tabs overflow-auto">
-            {activePlot !== undefined ? (
-                <PlotDetail plot={activePlot} contracts={contracts} injectedProvider={injectedProvider} mainnetProvider={mainnetProvider} />
-            )
-                :
-                (
-                    !userAddress ? (
-                        <div className="px-10 py-11">
-                            <LocationDetail />
-                            <TerrainDetail />
-                        </div>
-                    )
-                        :
-                        (
-                            activePlot !== undefined ? (
-                                <PlotDetail plot={activePlot} contracts={contracts} injectedProvider={injectedProvider} mainnetProvider={mainnetProvider} />
-                            ) : (
-                                <div className="py-6">
-                                    <PlotList
-                                        plots={newPlots.filter(item => userNft.includes(item.id))}
-                                        totalNum={userNft.length}
-                                        emptyMessage={"No plots have been purchased. You could be the first!"}
-                                    />
-                                </div>
-                            )
-                        )
-                )}
+  return (
+    <div className="plot-tabs overflow-auto">
+      {activePlot !== undefined ? (
+        <PlotDetail
+          plot={activePlot}
+          contracts={contracts}
+          injectedProvider={injectedProvider}
+          mainnetProvider={mainnetProvider}
+        />
+      ) : !userAddress ? (
+        <div className="px-10 py-11">
+          <LocationDetail />
+          <TerrainDetail />
         </div>
-    );
+      ) : activePlot !== undefined ? (
+        <PlotDetail
+          plot={activePlot}
+          contracts={contracts}
+          injectedProvider={injectedProvider}
+          mainnetProvider={mainnetProvider}
+        />
+      ) : (
+        <div className="py-6">
+          <PlotList plots={newPlots.filter(item => userNft.includes(item.id))} totalNum={userNft.length} />
+        </div>
+      )}
+    </div>
+  );
 }
