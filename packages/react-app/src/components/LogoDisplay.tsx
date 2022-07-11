2145b687a255e18d73760e442375a5fd87c9000a
Denis Rodman <blackeagle0188@gmail.com>
2022-07-11 22:27:15 +0900

    Fix logo title issue

--- packages/react-app/src/components/LogoDisplay.tsx
+++ packages/react-app/src/components/LogoDisplay.tsx
@@ -3,7 +3,7 @@ import Logo from "../assets/images/logo.png";
 
 export default function LogoDisplay() {
   return (
-    <div className="primary-font text-xl flex h-16 px-4 items-center">
+    <div className="primary-font text-lg sm:text-xl flex h-16 px-4 items-center">
       <img className="mr-4 h-8 bg-transparent" src={Logo} alt="CityDAO" />
       PARCEL 0
     </div>
